d3e1d33b0d568cb4f9a5b5018e6473868207034a
gerganasuv <gerganasuv@gmail.com>
2022-05-12 01:24:44 +0300

    Add project files.

---
+++ APITestsGitHub/APITestsGitHub.cs
@@ -0,0 +1,100 @@
+using NUnit.Framework;
+using RestSharp;
+using RestSharp.Authenticators;
+using RestSharp.Serializers.Json;
+using System.Collections.Generic;
+using System.Net;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace APITestsGitHub
+{
+    public class APITestsGitHub
+    {
+        private RestClient client;
+        [SetUp]
+        public void Setup()
+        {
+            this.client = new RestClient("https://api.github.com");
+            this.client.Authenticator = new HttpBasicAuthenticator("gerganasuv", "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");
+        }
+
+        #region Helper Methods
+
+        public async Task<Issue> CreateIssue(string title, string body)
+        {
+            //Arrange
+            var request = new RestRequest("/repos/TeamAlabala/CollectionTestsRepo/issues");
+
+            //Act
+            request.AddBody(new { body, title });
+            var responce = await this.client.ExecuteAsync(request, Method.Post);
+            var issue = JsonSerializer.Deserialize<Issue>(responce.Content);
+            return issue;
+
+        }
+
+        #endregion
+
+        [Test]
+        public async Task Test_GitHub_APIRequest()
+        {
+            //Arrange
+            var request = new RestRequest("/repos/TeamAlabala/CollectionTestsRepo/issues");
+
+            //Act
+            var responce = await this.client.ExecuteAsync(request);
+            var issues = JsonSerializer.Deserialize<List<Issue>>(responce.Content);
+
+            //Assert
+            Assert.That(issues.Count > 1);
+            foreach (var item in issues)
+            {
+                Assert.Greater(item.id, 0);
+                Assert.Greater(item.number, 0);
+                Assert.IsNotEmpty(item.title);
+            }
+
+            Assert.AreEqual(HttpStatusCode.OK, responce.StatusCode);
+
+        }      
+
+        [Test]
+        public async Task Test_Create_GitHubIssueAsync()
+        {
+            //Arrange
+            string title = "New issue from RestSharp";
+            string body = "Something";
+
+            //Act
+            var issue = await CreateIssue(title, body);
+
+            //Assert
+            Assert.Greater(issue.id, 0);
+            Assert.Greater(issue.number, 0);
+            Assert.IsNotEmpty(issue.title);
+        }
+
+        [TestCase("BG", "1000", "Sofija")]
+        [TestCase("BG", "8600", "Jambol")]
+        [TestCase("CA", "M5S", "Toronto")]
+        [TestCase("DE", "01067", "Dresden")]
+        [TestCase("GB", "B1", "Birmingham")]
+        public async Task TestZipopotamus(string countryCode,string zipCode,string expextedPlace)
+        {
+            //Arrange
+            client = new RestClient("https://api.zippopotam.us/");
+            var request = new RestRequest(countryCode+"/"+zipCode);
+
+            //Act
+            var responce = await this.client.ExecuteAsync(request,Method.Get);
+            var location = new SystemTextJsonSerializer().Deserialize<Location>(responce);
+
+            //Assert
+            Assert.AreEqual(HttpStatusCode.OK, responce.StatusCode);
+            Assert.AreEqual(countryCode,location.Abbreviation);
+            Assert.AreEqual(zipCode,location.PostCode);
+            StringAssert.Contains(expextedPlace, location.Places[0].PlaceName);
+        }
+    }
+}
