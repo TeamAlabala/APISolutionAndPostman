952146f24e5db6697d9234f3b5f51e64fee7b63b
DiLyana <105145475+DilYordanova@users.noreply.github.com>
2022-05-15 15:34:22 +0300

    Added tests

--- APITestsGitHub/APITestsGitHub.cs
+++ APITestsGitHub/APITestsGitHub.cs
@@ -1,13 +1,13 @@
 using NUnit.Framework;
 using RestSharp;
 using RestSharp.Authenticators;
-using RestSharp.Serializers.Json;
 using System.Collections.Generic;
 using System.IO;
 using System.Net;
 using System.Text.Json;
 using System.Threading.Tasks;
 using System;
+using RestSharp.Serializers.Json;
 
 namespace APITestsGitHub
 {
@@ -52,7 +52,7 @@ namespace APITestsGitHub
             var request = new RestRequest("/repos/TeamAlabala/APISolutionAndPostman/issues");
 
             //Act
-            var responce = await this.client.ExecuteAsync(request,Method.Get);
+            var responce = await this.client.ExecuteAsync(request, Method.Get);
             var issues = JsonSerializer.Deserialize<List<Issue>>(responce.Content);
 
             //Assert
@@ -66,7 +66,7 @@ namespace APITestsGitHub
 
             Assert.AreEqual(HttpStatusCode.OK, responce.StatusCode);
 
-        }      
+        }
 
         [Test]
         public async Task Test_Create_GitHubIssueAsync()
@@ -84,25 +84,98 @@ namespace APITestsGitHub
             Assert.IsNotEmpty(issue.title);
         }
 
+        [Test]
+        public async Task GitHubAPI_CreateNewIssue_MissingTitle()
+        {
+            var request = new RestRequest("/repos/TeamAlabala/APISolutionAndPostman/issues");
+            request.AddJsonBody(new
+            {
+                body = "some body",
+                label = new string[] { "bug", "importance:high", "type:UI" }
+            }
+            );
+
+            var response = await client.ExecuteAsync(request, Method.Post);
+
+            Assert.AreEqual(HttpStatusCode.UnprocessableEntity, response.StatusCode);
+        }
+
+        [Test]
+        public async Task GitHubAPI_CreateCommentIssue11()
+        {
+            var request = new RestRequest("/repos/TeamAlabala/APISolutionAndPostman/issues/11/comments");           
+            request.AddJsonBody(new
+            {
+                body = "Comment...."
+            }
+            );
+
+            var response = await client.ExecuteAsync(request, Method.Post);
+
+            Assert.AreEqual(HttpStatusCode.Created, response.StatusCode);
+        }
+
+        [Test]
+        public async Task GitHubAPI_RenameTitleIssue11()
+        {
+            var request = new RestRequest("/repos/TeamAlabala/APISolutionAndPostman/issues/11");           
+            request.AddJsonBody(new
+            {
+                title = "New title with RestSharp Nomber"
+            }
+            );
+         
+            var response = await client.ExecuteAsync(request, Method.Patch);
+            var issues = JsonSerializer.Deserialize<Issue>(response.Content);
+
+            Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+            Assert.That(issues.title.Contains("New title"));
+        }
+
+        [Test]
+        public async Task Test_GitHubAPI_DeleateComment()
+        {
+            //Firts create issue comment
+            var request = new RestRequest("/repos/TeamAlabala/APISolutionAndPostman/issues/11/comments");
+            request.AddJsonBody(new
+            {
+                body = "One more Comment........"
+            }
+            );
+
+            var response = await client.ExecuteAsync(request, Method.Post);
+
+            Assert.AreEqual(HttpStatusCode.Created, response.StatusCode);
+
+            var newComment = JsonSerializer.Deserialize<CommentResponse>(response.Content);
+
+            //Second delete issue comment
+
+            var requestDelete = new RestRequest("/repos/TeamAlabala/APISolutionAndPostman/issues/comments/" + newComment.id, Method.Delete);
+            var responseDelete = await client.ExecuteAsync(requestDelete);
+
+            Assert.AreEqual(HttpStatusCode.NoContent, responseDelete.StatusCode);
+        }
+
         [TestCase("BG", "1000", "Sofija")]
         [TestCase("BG", "8600", "Jambol")]
         [TestCase("CA", "M5S", "Toronto")]
         [TestCase("DE", "01067", "Dresden")]
         [TestCase("GB", "B1", "Birmingham")]
-        public async Task TestZipopotamus(string countryCode,string zipCode,string expextedPlace)
+        public async Task TestZipopotamus(string countryCode, string zipCode, string expextedPlace)
         {
             //Arrange
             client = new RestClient("https://api.zippopotam.us/");
-            var request = new RestRequest(countryCode+"/"+zipCode);
+            var request = new RestRequest(countryCode + "/" + zipCode);
 
             //Act
-            var responce = await this.client.ExecuteAsync(request,Method.Get);
+            var responce = await this.client.ExecuteAsync(request, Method.Get);
             var location = new SystemTextJsonSerializer().Deserialize<Location>(responce);
 
             //Assert
             Assert.AreEqual(HttpStatusCode.OK, responce.StatusCode);
-            Assert.AreEqual(countryCode,location.Abbreviation);
-            Assert.AreEqual(zipCode,location.PostCode);
+            Assert.AreEqual(countryCode, location.Abbreviation);
+            Assert.AreEqual(zipCode, location.PostCode);
             StringAssert.Contains(expextedPlace, location.Places[0].PlaceName);
         }
     }
